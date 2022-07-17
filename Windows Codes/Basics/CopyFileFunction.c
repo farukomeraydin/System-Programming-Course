c4df79bdc46bb5b511f187c4bae0be4fc9e3ab17
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2022-07-17 12:11:04 +0300

    CopyFile WINDOWS_API function

---
+++ Windows Codes/Basics/CopyFileFunction.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+
+void ExitSys(LPCSTR lpszMsg);
+
+int main(int argc, char* argv[]) {
+
+	if (!CopyFile("Sample.c", "kample.c", FALSE)) //If there is a kample file, it will fail. However, with the parameter FALSE, it will overwrite.
+		ExitSys("CopyFile");
+
+	printf("Ok\n");
+
+	return 0;
+}
+void ExitSys(LPCSTR lpszMsg)
+{
+	DWORD dwLastError = GetLastError();
+	LPTSTR lpszErr;
+
+	if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, dwLastError,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpszErr, 0, NULL)) {
+		fprintf(stderr, "%s (%lu): %s", lpszMsg, dwLastError, lpszErr);
+		LocalFree(lpszErr);
+	}
+
+	exit(EXIT_FAILURE);
+}
