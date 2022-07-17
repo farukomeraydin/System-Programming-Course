9c35e6a6b5ced396e8679cd7433d6406b95f16ec
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2022-07-17 12:15:42 +0300

    MoveFile WINDOWS_API function

---
+++ Windows Codes/Basics/MoveFile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+
+void ExitSys(LPCSTR lpszMsg);
+
+int main(int argc, char* argv[]) {
+
+	if (!MoveFile("Mample.c", "Tample.c", FALSE)) //If there is a Mample file, it will fail. However with FALSE parameter, it will overwrite.
+		ExitSys("CopyFile");
+
+	printf("Ok\n");
+
+	return 0;
+}
+void ExitSys(LPCSTR lpszMsg)
+{
+	DWORD dwLastError = GetLastError();
+	LPTSTR lpszErr;
+
+	if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, dwLastError,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&lpszErr, 0, NULL)) {
+		fprintf(stderr, "%s (%lu): %s", lpszMsg, dwLastError, lpszErr);
+		LocalFree(lpszErr);
+	}
+
+	exit(EXIT_FAILURE);
+}
