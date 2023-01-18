6844da4946c50a95288bbc4038c77dc0596ed429
Ömer Faruk <59957778+farukomeraydin@users.noreply.github.com>
2023-01-18 21:47:33 +0300

    change directory and getcwd

---
+++ Linux Codes/Basics/chdir_getcwd.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void exit_sys(const char* msg);
+
+int main(void){
+	char cwd[4096];
+
+	if (getcwd(cwd, 4096) == NULL)
+		exit_sys("getcwd");
+
+	puts(cwd);
+
+	if (chdir("/usr/include") == -1)
+		exit_sys("chdir");
+
+	if (getcwd(cwd, 4096) == NULL)
+		exit_sys("getcwd");
+	
+	puts(cwd);
+
+	return 0;
+}
+
+void exit_sys(const char* msg){
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
